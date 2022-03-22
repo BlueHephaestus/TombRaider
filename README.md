41ad9a9e290f7cf50f7c063db1d63151bc9516b9
blue <bluehephaestus@gmail.com>
2022-03-22 00:21:27 -0700

    README and updated for automated testdisk run to restore fs, verified with disk images.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Description
+
+Automated Imaging, Undeletion, Recovery, Indexing, Organizing, Forensics, and Crypto Salvaging for any hard drive.
+
+Given any hard drive, Tomb Raider will do everything in its power to recover as much data as possible on the drive, including searching through all partitions, all unallocated space, everything. It will then try to acquire the original structure of the data (the filesystem) and filenames, if the filesystem still is recoverable. Then, all OS files are filtered out so that only User files are left, these files are indexed and categorized, with anomalies or types of files laid out. Optionally, you can also search these files with a bitcoin salvager to test all private keys it finds.
+
+For any drive, this will take at most twice as much hard drive space on your computer to attempt to recover as much as possible, e.g. if you have a 500GB drive you're recovering, and it's completely full of data, you may need up to 1000GB (1TB) of storage to fully run this program in it's current iteration. 
+
+For any drive, this may take multiple days to run. The emphasis of this program is to make it fully automated, and as rigorous and meticulous as possible. No shortcuts, this bot puts first priority on recovering as much data as possible before filtering out any known irrelevant data (files that came with your OS, for example). 
--- tomb_raider.sh
+++ tomb_raider.sh
@@ -1,28 +1,56 @@
-#!/bin/bash -x
+#!/bin/bash +x
+# REMOVE WHEN DONE WITH DEV
+
 # Usage: ./tomb_raider <DRIVE>
 # Make sure you are in the directory you want your output stored.
 # THIS MAY TAKE SEVERAL DAYS TO COMPLETE!
-DRIVE=${1}
+drive=${1}
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
+# Make output directory
+#mkdir -p tombraider_output
+#cd tombraider_output
+
 
 # Safecopy - Imaging as much of drive as possible
+# From /dev/sdX to disk.img
 echo "Imaging drive $DRIVE to disk.img in current directory. DO NOT MOUNT YOUR DEVICE."
-safecopy --stage1 $DRIVE disk.img
-safecopy --stage2 $DRIVE disk.img
-safecopy --stage3 $DRIVE disk.img
+#safecopy --stage1 $DRIVE disk.img
+#safecopy --stage2 $DRIVE disk.img
+#safecopy --stage3 $DRIVE disk.img
 echo "Imaging Complete. You may now unplug your device."
+
+echo "Attempting to obtain Filesystem"
+# Get the filesystem on the drive to retain any file metadata and organization, if possible
+# MMLS - Get the number of allocated partitions on disk.img, if any (has to be alloc to have filesystem)
+# This is what we use to determine an upper bound for how many partitions will have recoverable fs's on testdisk.
+# tested via running it on 5 (if not more) of my own salvaged hard disks. This will include any boot files, etc.
+# More info in README
+fs_partitions=$((`mmls -a disk.img | wc -l`-5))
+echo "Found $fs_partitions Possible Filesystem Partitions on disk.img"
+
+i=1
+while [[ $i -le $fs_partitions ]]
+do
+  echo "Running Testdisk to attempt to recover files on partition $i/$fs_partitions"
+  testdisk /debug /log /cmd disk.img advanced,$i,list,filecopy
+  ((i++))
+done
+
+
+
+
 
 # Scalpel - Carving out parts that match private key hex patterns
 echo "Searching through ALL bytes on image for any matching private key patterns."
-scalpel disk.img
+#scalpel disk.img
 
 # Salvage - Check scalpel'd files for any keys that connect to an address with BTC balance.
 echo "Trying mutations and various formattings for found keys to check for BTC Balance."
 echo "Key Variants will appear as they are tried, and the program will exit with information EARLY if a balance is found. Good luck!"
-python3 bitcoin_salvager.py scalpel-output/
+#python3 bitcoin_salvager.py scalpel-output/
 
 echo "Happy Hunting! -Blue"
 
