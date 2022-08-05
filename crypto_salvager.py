da736ba16059f949b09009dfc2e639b4015ee13b
Blue <BlueHephaestus@gmail.com>
2022-08-04 22:07:22 -0700

    More fixes to crypto salvager

--- crypto_salvager.py
+++ crypto_salvager.py
@@ -1,13 +1,7 @@
-from bit import Key
-from bit.format import bytes_to_wif
 import re
 import sys
-import os
-import tqdm
-import mmap
 from tqdm import tqdm
 from filesystem_utils import sanitize
-import traceback
 
 # Format is ["Label", True/False (if regex), "Rule"]
 # NOTE: ALMOST ALL OF THESE ARE FROM AUTOPSY
@@ -18,7 +12,6 @@ ruleset = [
 ["Atomic Wallet", False, "atomic wallet.exe"],
 ["Bitcoin Armory", False, "armoryqt.exe"],
 ["Bitcoin Wallet", False, "bitcoin-qt.exe"],
-["Bitcoin Anything", False, "bitcoin"],
 ["Wallet Data File", False, "wallet.dat"], # mine
 ["Bither", False, "BitherWinLauncher.exe"],
 ["BitPay", False, "bitpay.exe"],
@@ -74,6 +67,13 @@ ruleset = [
 ["Zecwallet Lite", False, "Zecwallet Lite.exe"],
 ["Zel Core", False, "zelcore.exe"],
 ["Zel Core Portable", False, "zelcore-portable.exe"],
+
+# our own customs
+["Bitcoin Anything", False, "bitcoin"],
+["Dogecoin Anything", False, "dogecoin"],
+["Ethereum Anything", False, "ethereum"],
+["CryptoCurrency Anything", False, "cryptocurrency"],
+
 ]
 # Compile any regexes before using
 for i,rule in enumerate(ruleset):
@@ -103,104 +103,30 @@ def apply_ruleset(fpath):
 		print(f"MATCH FOUND: '{label}' with pattern '{pattern}' matched for filepath '{fpath}'")
 
 
-def salvage(priv):
-	# Given private key string candidate, mutate it and try different permutations to try and obtain a possible original key.
-	# priv: near-64 character string. (62, 63, or 64)
-	#
-	# Private key size can be either 32 or 31 bytes, end result. This means 62,63,or 64 characters in total - 3 possibilities
-	# each time they can also be either in wif compressed, wif uncompressed, or hex format, so that's 3 more possibilities.
-	# 
-	# So for a given key we will make 9 api calls, and each takes at most .2seconds, meaning that you can expect a runtime of 1.8s/key.
-	
-	# Start with cuts, pad it with zeros to give len 64 if needed
-	assert len(priv) in [62,63,64]
-	d = 64-len(priv)
-	priv += "0"*d
-
-	# more general but not needed and it's harder to read
-	#cuts = [priv[:64-i] for i in range(3):]
-
-	# 3 possible cutoffs.
-	cuts = [priv[:62], priv[:63], priv[:64]]
-
-	# Try each one on all parse methods
-	for priv in cuts:
-		try:
-			key = Key.from_hex(priv).to_bytes()
-		except ValueError:
-			# Invalid Key, skip.
-			continue
-		key_compressed = Key(bytes_to_wif(key, compressed=True))
-		key_uncompressed = Key(bytes_to_wif(key, compressed=False))
-
-		key_compressed_bal = float(key_compressed.get_balance('btc'))
-		key_uncompressed_bal = float(key_uncompressed.get_balance('btc'))
-		#tqdm.write(f"Key: {priv} Address: {key_compressed.address} Balance: {key_compressed_bal}")
-		#tqdm.write(f"Key: {priv} Address: {key_uncompressed.address} Balance: {key_uncompressed_bal}")
-		if key_compressed_bal != 0.0 or key_uncompressed_bal != 0.0:
-			print("#"*100)
-			print(f"Key: {priv} Address: {key_compressed.address} Balance: {key_compressed_bal}")
-			print(f"Key: {priv} Address: {key_uncompressed.address} Balance: {key_uncompressed_bal}")
-			print("#"*100)
-			# I know it's bad practice but this never happens anyways
-			sys.exit("JACKPOT! VALID PRIVATE KEY WITH BALANCE FOUND! EXITING EARLY...")
-		
-
-def fpaths(directory):
-	# Not a generator so tqdm likes it
-	l = []
-	for root,subdirs,files in os.walk(directory):
-		for f in files:
-			l.append(os.path.join(root,f))
-
-	return l
-
-def key_hex_candidates(fpath):
-	candidates = []
-	try:
-		with open(fpath, "rb") as f:
-			candidates= re.findall(b'\x01\x01\x04\x20(.{32})', f.read())
-	except MemoryError:
-		print(f"Encountered Very Large File {fpath} ({round(os.path.getsize(fpath)/1000000000, 2)} GB)\n \
-			  Attempting a Memory Mapping to read through it, this may take a moment.")
-		try:
-			with open(fpath, 'r+') as f:
-				data = mmap.mmap(f.fileno(), 0)
-				candidates = re.findall(b'\x01\x01\x04\x20(.{32})', data)
-			print("Successfully Read File, Continuing")
-		except:
-			print("Memory Map Read failed, skipping")
-
-	except OSError:
-		#print(f"Unknown error encountered with File {fpath}: {traceback.format_exc()}, skipping")
-		pass
-	return candidates
-
-
 if __name__ == "__main__":
-	# Given any directory, scour it for any trace of cryptocurrency usage and any private keys.
-	# Will check all files for any private key contents, and check all filenames for anything matching our rulesets.
-	data = sys.argv[1]
+	# Given any directory, scour it for any trace of cryptocurrency usage and / or crypto programs / keyfiles
+	# Optimized for speed and giving reliable indicators if there is something worth investigating on a given drive.
+	# Used to iterate through all files manually, and also check the contents of them for any possible private keys,
+	# then manually try those against the bitcoin API. This was very costly and didn't produce any results,
+	# so we're going with the much faster, flag-if-anything-comes-up (since this is rare) approach.
 
+	output_dir = sys.argv[1]
+	index_fpath = output_dir + "/" + "filesystem.index"
 	keys = set({})
 
 
-	print("Iterating through directory to check filenames against rulesets and obtain candidate private keys...")
-	for fpath in tqdm(fpaths(data)):
+	print("Checking Index Filepaths against Rulesets...")
+
+	with open(index_fpath, "r") as f:
+		# Iterate directly through it and check filenames line by line.
+		fpaths = [line.strip() for line in f.readlines()]
+
+	for line in tqdm(fpaths):
+		fpath = line.split(", ")[0]
 
 		# Check filepath against our rules
 		apply_ruleset(fpath)
 
-		# Get candidates
-		candidates = key_hex_candidates(fpath)
-		for priv in candidates:
-			keys.add(priv.hex())
-
-	print(f"Preprocessing complete, {len(keys)} Candidates Obtained. Testing...")
-	pbar = tqdm(keys)
-	for key in pbar:
-		pbar.set_description("Candidate: " + key)
-		salvage(key)
 
 
 
